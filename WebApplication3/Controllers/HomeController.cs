d23ecf99662a693764dc6636d6e0b3b8ce20ec7d
Sarika <sarika.d.gaikwad@gmail.com>
2021-01-22 12:29:33 +0530

    commit controller

--- WebApplication3/Controllers/HomeController.cs
+++ WebApplication3/Controllers/HomeController.cs
@@ -20,6 +20,13 @@ namespace WebApplication3.Controllers
             return View();
         }
 
+        public ActionResult Temp()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
